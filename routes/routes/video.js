95cb0cff1b7b549143217a9dbcf811fb39a30ae9
RaiyanB <157034172+RaiyanB@users.noreply.github.com>
2025-05-05 00:01:45 -0400

    Delete routes/routes directory

--- routes/routes/video.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const videoController = require('../controllers/videoController');
-
-router.get('/new_video', videoController.getNewVideo);
-router.post('/new', videoController.postNewVideo);
-router.get('/dashboard/:filter', videoController.getDashboard);
-
-module.exports = router;
